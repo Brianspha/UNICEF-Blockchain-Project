07bd8f7af2697122fb9a1882a44842560f004f2c
Pape Alioune <papealioune@MacBook-Pro-de-me.local>
2019-07-31 23:03:28 +0000

    Token contract creation

--- contracts/Token.sol
+++ contracts/Token.sol
@@ -14,22 +14,5 @@ contract Token {
 
     // Events
     event Transfer(address indexed from, address indexed to, uint256 value);
-
-    constructor() public {
-        totalSupply = 1000000 * (10 ** decimals);
-        balanceOf[msg.sender] = totalSupply;
-    }
-    // Methodes
-    function transfer(address _to, uint256 _value) public returns (bool success) {
-        require(balanceOf[msg.sender] >= _value);
-        _transfer(msg.sender, _to, _value);
-        return true;
-    }
-
-    function _transfer(address _from, address _to, uint256 _value) internal {
-        require(_to != address(0));
-        balanceOf[_from] = balanceOf[_from].sub(_value);
-        balanceOf[_to] = balanceOf[_to].add(_value);
-        emit Transfer(_from, _to, _value);
-    }
+    
 }
