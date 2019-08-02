793d9be31b55f4292de62e569f7d9bdd7e0d9524
Pape Alioune <papealioune@MacBook-Pro-de-me.local>
2019-08-02 03:02:09 +0000

    add new dependencies

--- contracts/Token.sol
+++ contracts/Token.sol
@@ -19,17 +19,4 @@ contract Token {
         totalSupply = 1000000 * (10 ** decimals);
         balanceOf[msg.sender] = totalSupply;
     }
-    // Methodes
-    function transfer(address _to, uint256 _value) public returns (bool success) {
-        require(balanceOf[msg.sender] >= _value);
-        _transfer(msg.sender, _to, _value);
-        return true;
-    }
-
-    function _transfer(address _from, address _to, uint256 _value) internal {
-        require(_to != address(0));
-        balanceOf[_from] = balanceOf[_from].sub(_value);
-        balanceOf[_to] = balanceOf[_to].add(_value);
-        emit Transfer(_from, _to, _value);
-    }
 }
